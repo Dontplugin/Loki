70ae0c67f7c41f1008a6a8103fc590707eb97a99
韩宇哲 <hanyuzhe@souche.com>
2019-06-19 16:51:40 +0800

    git init

---
+++ src/main/java/top/sinfulxx/loki/LokiApplication.java
@@ -0,0 +1,14 @@
+package top.sinfulxx.loki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LokiApplication.class, args);
+    }
+
+}
